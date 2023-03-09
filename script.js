babed66bdba930d35a6df6c48fc3b8957fd4fe92
csturgeon <sturgeon.chris@gmail.com>
2023-03-09 16:19:45 +0000

    Add timings and complete refactor of HTML, CSS and JS

--- script.js
+++ script.js
@@ -1,49 +1,129 @@
 const launch = new Date(Date.now() + 1000 * 60 * 60 * 24 * 14);
 
-// DOING - establish previous figs to compare within each loop
-// to see if the tiles need flicking
-let prevDays;
-let prevHours;
-let prevMinutes;
-let p;
+// Days variables
+const daysTop = document.querySelector('.top[unit=days]');
+const daysTopWrapper = document.querySelector('.wrapper[unit=days]');
+const daysBottom = document.querySelector('.bottom[unit=days]');
+let prevDays = 14;
+
+// Hours Variables
+const hoursTop = document.querySelector('.top[unit=hours]');
+const hoursTopWrapper = document.querySelector('.wrapper[unit=hours]');
+const hoursBottom = document.querySelector('.bottom[unit=hours]');
+let prevHours = 0;
+
+// Minutes Variables
+const minutesTop = document.querySelector('.top[unit=minutes]');
+const minutesTopWrapper = document.querySelector('.wrapper[unit=minutes]');
+const minutesBottom = document.querySelector('.bottom[unit=minutes]');
+let prevMinutes = 0;
+
+// Seconds Variables
+const secondsTop = document.querySelector('.top[unit=seconds]');
+const secondsTopWrapper = document.querySelector('.wrapper[unit=seconds]');
+const secondsBottom = document.querySelector('.bottom[unit=seconds]');
+let prevSeconds = 0;
 
 function showTime() {
-  const bottoms = document.querySelectorAll('.bottom');
-  bottoms.forEach((bottom) => {
-    bottom.classList.add('darker');
-
-    setTimeout(() => {
-      bottom.classList.remove('darker');
-    }, 100);
-  });
-
-  const top = document.querySelector('.top');
-  top.classList.remove('flip');
-
-  // const bottom = document.querySelector('.bottom');
-  // bottom.classList.add('darker');
-
+  // Calculate difference in time milliseconds from when timer script started
   const now = new Date();
   const difference = launch.getTime() - now.getTime();
+
+  // Update days
   const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  if (days !== prevDays) {
+    prevDays = days;
+    daysTop.classList.add('drop');
+    daysTopWrapper.classList.add('flip');
+
+    setTimeout(() => {
+      document.querySelector('#days-top').innerText = `${days}`;
+      document.querySelector('#days-bottom').innerText = `${days}`;
+      daysTop.classList.remove('drop');
+      daysTopWrapper.classList.remove('flip');
+      daysBottom.classList.add('darker');
+
+      setTimeout(() => {
+        daysBottom.classList.remove('darker');
+      }, 100);
+    }, 100);
+  } else {
+    document.querySelector('#days-top').innerText = `${days}`;
+    document.querySelector('#days-bottom').innerText = `${days}`;
+  }
+
+  // Update hours
   const hours = Math.floor(difference / (1000 * 60 * 60)) % 24;
+  if (prevHours !== hours) {
+    prevHours = hours;
+    hoursTop.classList.add('drop');
+    hoursTopWrapper.classList.add('flip');
+
+    setTimeout(() => {
+      document.querySelector('#hours-top').innerText = `${hours}`;
+      document.querySelector('#hours-bottom').innerText = `${hours}`;
+      hoursTop.classList.remove('drop');
+      hoursTopWrapper.classList.remove('flip');
+      hoursBottom.classList.add('darker');
+
+      setTimeout(() => {
+        hoursBottom.classList.remove('darker');
+      }, 100);
+    }, 100);
+  } else {
+    document.querySelector('#hours-top').innerText = `${hours}`;
+    document.querySelector('#hours-bottom').innerText = `${hours}`;
+  }
+
+  // Update minutes
   const minutes = Math.floor(difference / (1000 * 60)) % 60;
+  if (prevMinutes !== minutes) {
+    prevMinutes = minutes;
+    minutesTopWrapper.classList.add('flip');
+    minutesTop.classList.add('drop');
+
+    setTimeout(() => {
+      document.querySelector('#minutes-top').innerText = `${minutes}`;
+      document.querySelector('#minutes-bottom').innerText = `${minutes}`;
+      minutesTopWrapper.classList.remove('flip');
+      minutesTop.classList.remove('drop');
+      minutesBottom.classList.add('darker');
+
+      setTimeout(() => {
+        minutesBottom.classList.remove('darker');
+      }, 100);
+    }, 100);
+  } else {
+    document.querySelector('#minutes-top').innerText = `${minutes}`;
+    document.querySelector('#minutes-bottom').innerText = `${minutes}`;
+  }
+
+  // Update seconds
   const seconds = Math.ceil(difference / 1000) % 60;
+  if (seconds !== prevSeconds) {
+    prevSeconds = seconds;
+    secondsTopWrapper.classList.add('flip');
+    secondsTop.classList.add('drop');
 
-  document.querySelector('#days').innerHTML = `${days} days`;
-  document.querySelector('#hours').innerHTML = `${hours} hours`;
-  document.querySelector('#minutes').innerHTML = `${minutes} minutes`;
-  document.querySelector('#seconds-top').innerText = `${seconds}`;
-  document.querySelector('#seconds-bottom').innerText = `${seconds}`;
+    setTimeout(() => {
+      document.querySelector('#seconds-top').innerText = `${seconds}`;
+      document.querySelector('#seconds-bottom').innerText = `${seconds}`;
+      secondsTopWrapper.classList.remove('flip');
+      secondsTop.classList.remove('drop');
+      secondsBottom.classList.add('darker');
 
-  setTimeout(() => {
-    const top = document.querySelector('.top');
-    top.classList.add('flip');
-  }, 900);
+      setTimeout(() => {
+        secondsBottom.classList.remove('darker');
+      }, 100);
+    }, 100);
+  } else {
+    document.querySelector('#seconds-top').innerText = `${seconds}`;
+    document.querySelector('#seconds-bottom').innerText = `${seconds}`;
+  }
 }
 
+// Run function on start and set interval for each second
 showTime();
-
 setInterval(() => {
   showTime();
 }, 1000);
